3dac529b87c97bf97e36c29eba2994305aa261ee
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-09-28 17:13:32 -0700

    update category as array

--- models/product.js
+++ models/product.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const ProductSchema = new Schema({
     name: { type: String, required: true, maxLength: 100 },
     description: { type: String, required: true },
-    category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
+    category: [{ type: Schema.Types.ObjectId, ref: "Category", required: true }],
     price: { type: Number, required: true },
     quantity_in_stock: { type: Number, required: true }
 });
